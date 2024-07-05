91f7699cab377d58aa00598a052122c0bc072ff3
subrat <subrat.panigrahi@paytm.com>
2024-07-06 01:57:25 +0530

    cors enable

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const cors = require('cors');
 const port = process.env.PORT || 3000;
 const indexRouter = require('./routes/index.js');
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 app.use('/', indexRouter);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
