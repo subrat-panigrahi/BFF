682e20731cb5a89104ed3b38e33d95cb7e75b585
subrat <subrat.panigrahi@paytm.com>
2024-07-06 01:52:15 +0530

    cors enable

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 const indexRouter = require('./routes/index.js');
 
+app.use(cors());
 app.use('/', indexRouter);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
